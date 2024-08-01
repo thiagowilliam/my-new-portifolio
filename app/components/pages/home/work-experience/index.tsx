e6c8bef5c9bff663f0b263c9d5c518f70640d881
Thiago william <thiago.will.silva@gmail.com>
2024-08-01 15:40:34 -0300

    feat: work experience text

--- app/components/pages/home/work-experience/index.tsx
+++ app/components/pages/home/work-experience/index.tsx
@@ -12,10 +12,7 @@ export const WorkExperience = ({experiencies} : WorkExperienceProps) => {
       <div className="max-w-[420px]">
         <SectionTitle subtitle="experiências" title="Experiência Profissonal" />
         <p className="text-gray-400 mt-6">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nobis
-          provident pariatur saepe suscipit quibusdam, mollitia unde nesciunt
-          explicabo vel excepturi culpa autem. Dolorem, corrupti neque unde
-          perferendis quas recusandae!
+          Planejar e executar um projeto do início ao fim é uma grande satisfação para mim, sempre visando entregar interfaces que proporcionem uma experiência única e agradável para o usuário.
         </p>
       </div>
 
